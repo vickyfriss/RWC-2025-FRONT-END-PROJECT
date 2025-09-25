84d5cee415b2ba924f580f3602974273029e2b90
Victoria Friss de Kereki <vicky_friss@hotmail.com>
2025-09-25 12:57:28 +0100

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,11 @@
 import { fileURLToPath, URL } from 'node:url'
-
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/', // ✅ add this for Netlify
   plugins: [
     vue(),
     vueDevTools(),
