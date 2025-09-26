e2235dd416f1e0082f00d7192db0a57199e75665
Victoria Friss de Kereki <vicky_friss@hotmail.com>
2025-09-26 18:07:32 +0100

    Update my-first-test.cy.js

--- cypress/e2e/my-first-test.cy.js
+++ cypress/e2e/my-first-test.cy.js
@@ -1,7 +1,36 @@
 describe('My Vue App', () => {
-    it('loads the home page', () => {
-      cy.visit('/')               // goes to baseUrl from cypress.config.js
-      cy.get('body').should('exist')  // simplest check
-    })
+
+  beforeEach(() => {
+    cy.visit('/')  // visit home page before every test
   })
-  
+
+  it('loads the home page', () => {
+    cy.get('body').should('exist')
+    cy.get('img[alt="Women Rugby World Cup Logo"]').should('exist')
+    cy.get('h1.text-h3.font-weight-bold').should('exist')
+    cy.contains('button', 'Login').should('be.visible')
+  })
+
+  it('renders all pool names', () => {
+    cy.contains('Pool A').should('be.visible')
+    cy.contains('Pool B').should('be.visible')
+    cy.contains('Pool C').should('be.visible')
+    cy.contains('Pool D').should('be.visible')
+  })
+
+  it('renders at least one team in Pool A', () => {
+    cy.contains('Pool A')
+      .parent()
+      .find('.team-name')
+      .should('exist')
+      .and('contain.text', 'England')
+  })
+
+  it('shows the Host Cities & Venues heading and input', () => {
+    cy.contains('h2', 'Host Cities & Venues').should('exist')
+    cy.get('input[placeholder="Select city or stadium..."]').should('exist')
+    cy.get('.leaflet-container').should('exist') // the main map div
+    cy.get('.leaflet-tile-loaded').should('have.length.greaterThan', 0) // at least one map tile loaded
+  })
+
+})
