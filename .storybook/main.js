8916729834d31f820260bd6b2f4f2a16c1e8c791
Victoria Friss de Kereki <vicky_friss@hotmail.com>
2025-09-28 16:47:37 +0100

    -

---
+++ .storybook/main.js
@@ -0,0 +1,14 @@
+
+
+/** @type { import('@storybook/vue3-vite').StorybookConfig } */
+const config = {
+  "stories": [
+    "../src/**/*.stories.@(js|jsx|mjs|ts|tsx)"
+  ],
+  "addons": [],
+  "framework": {
+    "name": "@storybook/vue3-vite",
+    "options": {}
+  }
+};
+export default config;
